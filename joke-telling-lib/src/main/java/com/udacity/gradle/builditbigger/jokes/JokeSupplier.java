b6a429eeb6ebdce2cc0f601337d37d9d20501008
Maximilian Frick <maximilian.frick@intive-kupferwerk.com>
2016-12-12 18:00:18 +0100

    step 1 add library

---
+++ joke-telling-lib/src/main/java/com/udacity/gradle/builditbigger/jokes/JokeSupplier.java
@@ -0,0 +1,8 @@
+package com.udacity.gradle.builditbigger.jokes;
+
+public class JokeSupplier {
+
+    public static String getJoke() {
+        return "What is brown and swims in the sea? \n - A subbread!";
+    }
+}
