20c902cd8df34e4bfdab3666259d17ec0f37b1bd
Maximilian Frick <maximilian.frick@intive-kupferwerk.com>
2016-12-12 18:31:33 +0100

    step 2 add android library

---
+++ joke-telling-lib-android/src/main/java/com/udacity/gradle/builditbigger/android/jokes/JokeTellingActivity.java
@@ -0,0 +1,35 @@
+package com.udacity.gradle.builditbigger.android.jokes;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class JokeTellingActivity extends AppCompatActivity {
+
+    public static final String EXTRA_JOKE = "EXTRA_JOKE";
+
+    public static Intent newIntent(Context context, String joke) {
+        Intent intent = new Intent(context, JokeTellingActivity.class);
+        intent.putExtra(EXTRA_JOKE, joke);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new AlertDialog.Builder(this)
+                .setMessage(getIntent().getStringExtra(EXTRA_JOKE))
+                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        finish();
+                    }
+                })
+                .show();
+    }
+}
